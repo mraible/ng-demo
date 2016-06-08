11351db52d0510504037b22bd34a90e5b3bb8945
Matt Raible <matt@raibledesigns.com>
2016-06-08 16:04:04 +0100

    Fixed test

--- src/app/+search/search.component.spec.ts
+++ src/app/+search/search.component.spec.ts
@@ -14,33 +14,31 @@ import {MockSearchService} from '../shared/search/mocks/search.service';
 
 import {SearchComponent} from './search.component';
 
-export function main() {
-  describe('Search component', () => {
-    var mockSearchService:MockSearchService;
+describe('Search component', () => {
+  var mockSearchService:MockSearchService;
 
-    beforeEachProviders(() => {
-      mockSearchService = new MockSearchService();
+  beforeEachProviders(() => {
+    mockSearchService = new MockSearchService();
 
-      return [
-        mockSearchService.getProviders(),
-        provide(RouteSegment, {useValue: new MockRouteSegment({'term': 'peyton'})})
-      ];
-    });
-
-    it('should search when a term is set and search() is called', inject([TestComponentBuilder], (tcb:TestComponentBuilder) => {
-      return tcb.createAsync(SearchComponent).then((fixture) => {
-        let searchComponent = fixture.debugElement.componentInstance;
-        searchComponent.query = 'M';
-        searchComponent.search();
-        expect(mockSearchService.searchSpy).toHaveBeenCalledWith('M');
-      });
-    }));
-
-    it('should search automatically when a term is on the URL', inject([TestComponentBuilder], (tcb:TestComponentBuilder) => {
-      return tcb.createAsync(SearchComponent).then((fixture) => {
-        fixture.detectChanges();
-        expect(mockSearchService.searchSpy).toHaveBeenCalledWith('peyton');
-      });
-    }));
+    return [
+      mockSearchService.getProviders(),
+      provide(RouteSegment, {useValue: new MockRouteSegment({'term': 'peyton'})})
+    ];
   });
-}
+
+  it('should search when a term is set and search() is called', inject([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+    return tcb.createAsync(SearchComponent).then((fixture) => {
+      let searchComponent = fixture.debugElement.componentInstance;
+      searchComponent.query = 'M';
+      searchComponent.search();
+      expect(mockSearchService.searchSpy).toHaveBeenCalledWith('M');
+    });
+  }));
+
+  it('should search automatically when a term is on the URL', inject([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+    return tcb.createAsync(SearchComponent).then((fixture) => {
+      fixture.detectChanges();
+      expect(mockSearchService.searchSpy).toHaveBeenCalledWith('peyton');
+    });
+  }));
+});
