4e7ce59d6f93cd86ea61ca3dde78950e962a05af
Matt Raible <matt@raibledesigns.com>
2016-12-06 13:25:14 -0700

    Upgrade to Angular CLI v1.0.0-beta.22-1

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -5,8 +5,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
-  templateUrl: 'edit.component.html',
-  styleUrls: ['edit.component.css']
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit, OnDestroy {
   person: Person;
