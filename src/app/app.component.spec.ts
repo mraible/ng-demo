94c5b6d02084a58794d616d2de829425c8145bd6
Matt Raible <matt@raibledesigns.com>
2017-01-05 17:51:48 -0800

    Show first test failing by default, like the tutorial does

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,13 +2,11 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      imports: [RouterTestingModule]
+      declarations: [AppComponent]
     });
     TestBed.compileComponents();
   });
