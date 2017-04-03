ed66238f27c00a5348c5f727e733d697cffb89f7
Matt Raible <matt@raibledesigns.com>
2017-04-03 16:20:43 -0600

    Add login example using Okta Auth JS

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,47 @@
 import { Component } from '@angular/core';
 import { OAuthService } from 'angular-oauth2-oidc';
+declare let OktaAuth: any;
 
 @Component({
   template: `<div *ngIf="givenName">
 <h2>Welcome, {{givenName}}!</h2>
-<button (click)="logout()">Logout</button>
+<button (click)="logout()" class="btn btn-default">Logout</button>
 <p><a routerLink="/search" routerLinkActive="active">Search</a></p>
 </div>
 
-<div *ngIf="!givenName">
-    <button (click)="login()">Login</button>
+<div class="panel panel-default" *ngIf="!givenName">
+    <div class="panel-body">
+        <p>Login with Authorization Server</p>
+        <button class="btn btn-default" (click)="login()">Login</button>
+    </div>
+</div>
+
+<div class="panel panel-default" *ngIf="!givenName">
+    <div class="panel-body">
+        <p>Login with Username/Password</p>
+
+        <p style="color:red; font-weight:bold" *ngIf="loginFailed">
+            Login wasn't successful.
+        </p>
+
+        <div class="form-group">
+            <label>Username</label>
+            <input class="form-control" [(ngModel)]="userName">
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <input class="form-control" type="password" [(ngModel)]="password">
+        </div>
+        <div class="form-group">
+            <button class="btn btn-default" (click)="loginWithPassword()">Login</button>
+        </div>        
+    </div>
 </div>`
 })
 export class HomeComponent {
+  userName;
+  password;
+
   constructor(private oauthService: OAuthService) {
   }
 
@@ -32,4 +61,39 @@ export class HomeComponent {
     return claims.name;
   }
 
+  loginWithPassword() {
+    const noonce = this.oauthService.createAndSaveNonce().then(nonce => {
+      console.log('nonce', nonce);
+      const authClient = new OktaAuth({
+        url: 'https://dev-158606.oktapreview.com'
+      });
+      authClient.signIn({
+        username: this.userName,
+        password: this.password
+      }).then((response) => {
+        if (response.status === 'SUCCESS') {
+          console.log('success', response);
+          authClient.token.getWithoutPrompt({
+            clientId: 'RqjWvpvWO77qMGgDfukY',
+            responseType: ['id_token', 'token'],
+            scopes: ['openid', 'profile', 'email'],
+            sessionToken: response.sessionToken,
+            nonce: nonce,
+            redirectUri: window.location.origin
+          })
+            .then((tokens) => {
+              this.oauthService.processIdToken(tokens[0].idToken, tokens[1].accessToken);
+              // todo: figure out how to re-invoke the call to givenName
+              // by the template w/o reloading the page
+              location.reload();
+            })
+            .catch(error => console.error(error));
+        } else {
+          throw new Error('We cannot handle the ' + response.status + ' status');
+        }
+      }).fail(function (err) {
+        console.error(err);
+      });
+    });
+  }
 }
