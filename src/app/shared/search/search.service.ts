824eb31d50cad707aa9b15f9cf21c4ea0a98df5d
Matt Raible <matt.raible@okta.com>
2018-03-13 14:31:42 -0600

    Update to test-start branch

--- src/app/shared/search/search.service.ts
+++ src/app/shared/search/search.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class SearchService {
-
-  constructor(private http: Http) {}
+  constructor(private http: HttpClient) {}
 
   getAll() {
-    return this.http.get('assets/data/people.json').map((res: Response) => res.json());
+    return this.http.get('assets/data/people.json');
   }
 
   search(q: string): Observable<any> {
