2cccef49d38873324d8387cbbe7d8efe5ef1be8e
Matt Raible <matt.raible@okta.com>
2018-05-07 20:41:27 -0600

    Update Angular Material branch to Angular 6

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,15 +2,14 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatListModule, MatToolbarModule } from '@angular/material';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
-      imports: [
-        RouterTestingModule, MatListModule, MatToolbarModule
-      ]
+      imports: [RouterTestingModule, MatListModule, MatToolbarModule]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { SearchService } from './shared';
 import { EditComponent } from './edit/edit.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatIconModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
@@ -24,17 +24,17 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
     MatInputModule,
     MatListModule,
-    MatToolbarModule,
-    RouterModule.forRoot(appRoutes)
+    MatToolbarModule
   ],
-  providers: [SearchService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
