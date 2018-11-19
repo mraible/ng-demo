82d62d6084abac8d7016680df29f5db07b09c549
Matt Raible <matt.raible@okta.com>
2018-11-19 10:29:18 -0700

    Upgrade Bootstrap branch to Angular 7 (#47)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ng-demo');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title in a nav tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-demo!');
+    expect(compiled.querySelector('nav').textContent).toContain('Welcome to ng-demo!');
   });
 });
