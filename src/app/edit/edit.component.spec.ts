7d3197a14be325cac011915605ab4a62911366dd
Matt Raible <matt.raible@okta.com>
2020-09-04 19:41:11 -0600

    Update tutorial through testing

--- src/app/edit/edit.component.spec.ts
+++ src/app/edit/edit.component.spec.ts
@@ -1,54 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { EditComponent } from './edit.component';
-import { TestBed } from '@angular/core/testing';
-import { Address, Person, SearchService } from '../shared';
-import { MockRouter, MockActivatedRoute } from '../shared/search/mocks/routes';
-import { ActivatedRoute, Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { of } from 'rxjs';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('EditComponent', () => {
-  let mockSearchService: SearchService;
-  let mockActivatedRoute: MockActivatedRoute;
-  let mockRouter: MockRouter;
+  let component: EditComponent;
+  let fixture: ComponentFixture<EditComponent>;
 
-  beforeEach(() => {
-    mockActivatedRoute = new MockActivatedRoute({id: 1});
-    mockRouter = new MockRouter();
-
-    TestBed.configureTestingModule({
-      declarations: [EditComponent],
-      providers: [
-        {provide: ActivatedRoute, useValue: mockActivatedRoute},
-        {provide: Router, useValue: mockRouter}
-      ],
-      imports: [FormsModule, HttpClientTestingModule]
-    }).compileComponents();
-
-    mockSearchService = TestBed.inject(SearchService);
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EditComponent ]
+    })
+    .compileComponents();
   });
 
-  it('should fetch a single record', () => {
-    const fixture = TestBed.createComponent(EditComponent);
-
-    const person = new Person({id: 1, name: 'Gary Harris'});
-    person.address = new Address({city: 'Denver'});
-
-    // mock response
-    spyOn(mockSearchService, 'get').and.returnValue(of(person));
-
-    // initialize component
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EditComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
+  });
 
-    // verify service was called
-    expect(mockSearchService.get).toHaveBeenCalledWith(1);
-
-    // verify data was set on component when initialized
-    const editComponent = fixture.debugElement.componentInstance;
-    expect(editComponent.editAddress.city).toBe('Denver');
-
-    // verify HTML renders as expected
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h3').innerHTML).toBe('Gary Harris');
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });
--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,38 +1,19 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchComponent } from './search.component';
-import { MockActivatedRoute } from '../shared/search/mocks/routes';
-import { SearchService } from '../shared';
-import { ActivatedRoute } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { RouterTestingModule } from '@angular/router/testing';
-import { of } from 'rxjs';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
-  let mockActivatedRoute: MockActivatedRoute;
-  let mockSearchService: SearchService;
 
-  beforeEach(async(() => {
-    mockActivatedRoute = new MockActivatedRoute({term: 'nikola'});
-
-    TestBed.configureTestingModule({
-      declarations: [SearchComponent],
-      providers: [
-        {provide: ActivatedRoute, useValue: mockActivatedRoute}
-      ],
-      imports: [FormsModule, RouterTestingModule, HttpClientTestingModule]
-    }).compileComponents();
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SearchComponent ]
+    })
+    .compileComponents();
+  });
 
   beforeEach(() => {
-    // mock response
-    mockSearchService = TestBed.inject(SearchService);
-    mockSearchService.search = jasmine.createSpy().and.returnValue(of([]));
-
-    // initialize component
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -40,17 +21,5 @@ describe('SearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should search when a term is set and search() is called', () => {
-    component = fixture.debugElement.componentInstance;
-    component.query = 'J';
-    component.search();
-    expect(mockSearchService.search).toHaveBeenCalledWith('J');
-  });
-
-  it('should search automatically when a term is on the URL', () => {
-    fixture.detectChanges();
-    expect(mockSearchService.search).toHaveBeenCalledWith('nikola');
   });
 });
