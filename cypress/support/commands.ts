4816fb3eb40acecd3809226f79e93bb510302a2c
Matt Raible <matt@raibledesigns.com>
2023-02-16 15:11:32 -0700

    Try using cy.origin()

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,5 +1,20 @@
 Cypress.Commands.add('signIn', (username, password) => {
-  cy.get('input[name=username]').type(username);
-  cy.get('input[name=password]').type(password);
-  cy.get('button[type=submit]').first().click();
+  const log = Cypress.log({
+    displayName: 'AUTH0 LOGIN',
+    message: [`🔐 Authenticating | ${username}`],
+    // @ts-ignore
+    autoEnd: false,
+  })
+
+  cy.origin(
+    Cypress.env('E2E_DOMAIN'),
+    {args: {username, password}},
+    ({username, password}) => {
+      cy.get('input[name=username]').type(username);
+      cy.get('input[name=password]').type(password, {log: false});
+      cy.get('button[type=submit]').first().click();
+    }
+  )
+
+  cy.url().should('equal', 'http://localhost:4200/home')
 })
