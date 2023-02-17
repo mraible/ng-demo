6bbf7a153a2be93b243092af41ca5abf0fe86c0e
Matt Raible <matt@raibledesigns.com>
2023-02-16 17:02:17 -0700

    Fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,7 +29,7 @@ describe('AppComponent', () => {
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('nav').textContent).toContain('Welcome to ng-demo!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('nav')?.textContent).toContain('Welcome to ng-demo!');
   });
 });
