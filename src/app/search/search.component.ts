355608169b0a8f141d216ceaa784d2f28127dd38
Matt Raible <matt@raibledesigns.com>
2023-12-02 23:15:01 -0700

    Fixing and smiling

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -7,11 +7,12 @@ import { Subscription } from 'rxjs';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [FormsModule, JsonPipe, RouterLink, MatFormFieldModule, MatInputModule, MatIconModule],
+  imports: [FormsModule, JsonPipe, RouterLink, MatFormFieldModule, MatInputModule, MatIconModule, MatListModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
