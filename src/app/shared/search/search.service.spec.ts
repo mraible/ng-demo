34aac1c1d43f57f225d6b6c61e4979c92bd48234
Matt Raible <matt@raibledesigns.com>
2023-12-02 23:44:03 -0700

    Fix formatting

--- src/app/shared/search/search.service.spec.ts
+++ src/app/shared/search/search.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { SearchService } from './search.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
