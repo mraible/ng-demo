5ffedd53d90fd982de3d9afc2fe3faab46fc8f4f
Matt Raible <matt@raibledesigns.com>
2023-12-03 08:43:48 -0700

    Import MatButtonModule

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -8,11 +8,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [FormsModule, JsonPipe, RouterLink, MatFormFieldModule, MatInputModule, MatIconModule, MatListModule],
+  imports: [FormsModule, JsonPipe, RouterLink, MatFormFieldModule, MatInputModule, MatIconModule, MatListModule, MatButtonModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
