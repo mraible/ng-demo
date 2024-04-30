7cf38fe69b9c491b1461918a71c1ccf484769edf
Matt Raible <matt@raibledesigns.com>
2024-04-30 12:18:40 -0600

    Upgrade to Angular Material 17.3.6

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideAnimations()]
+  providers: [provideRouter(routes), provideHttpClient(), provideAnimations(), provideAnimationsAsync()]
 };
